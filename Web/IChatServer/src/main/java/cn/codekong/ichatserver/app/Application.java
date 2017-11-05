03f54511b4bd27247d9253fadecab14e69353caa
codekongs <531173160@qq.com>
2017-11-05 22:37:53 +0800

    添加工具类,设置数据库映射

--- Web/IChatServer/src/main/java/cn/codekong/ichatserver/app/Application.java
+++ Web/IChatServer/src/main/java/cn/codekong/ichatserver/app/Application.java
@@ -1,10 +1,10 @@
 package cn.codekong.ichatserver.app;
 
-import com.fasterxml.jackson.jaxrs.json.JacksonJsonProvider;
-
 import org.glassfish.jersey.server.ResourceConfig;
 
 import java.util.logging.Logger;
+
+import cn.codekong.ichatserver.provider.GsonProvider;
 
 /**
  * Created by 尚振鸿 on 17-10-6. 14:20
@@ -17,7 +17,9 @@ public class Application extends ResourceConfig{
         //注册逻辑处理的包名
         //packages(AccountService.class.getPackage().getName());
         //注册json解析器
-        register(JacksonJsonProvider.class);
+        //register(JacksonJsonProvider.class);
+        //替换为Gson解析器
+        register(GsonProvider.class);
         //注册日志打印
         register(Logger.class);
     }
