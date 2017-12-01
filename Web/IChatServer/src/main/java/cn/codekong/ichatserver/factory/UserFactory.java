fbc591fcebc3651bcfc3a6909405c611dd75edaf
codekongs <531173160@qq.com>
2017-12-01 22:25:07 +0800

    基本的登录注册逻辑规范完成

--- Web/IChatServer/src/main/java/cn/codekong/ichatserver/factory/UserFactory.java
+++ Web/IChatServer/src/main/java/cn/codekong/ichatserver/factory/UserFactory.java
@@ -1,6 +1,6 @@
 package cn.codekong.ichatserver.factory;
 
-import org.hibernate.Session;
+import java.util.UUID;
 
 import cn.codekong.ichatserver.bean.db.User;
 import cn.codekong.ichatserver.utils.Hib;
@@ -12,6 +12,16 @@ import cn.codekong.ichatserver.utils.TextUtil;
  */
 
 public class UserFactory {
+    //通过token字段查询用户信息
+    //只能自己使用,查询个人信息,非他人信息
+    public static User findByToken(String token){
+        return Hib.query(session -> (User) session
+                .createQuery("from User where token=:token")
+                .setParameter("token", token)
+                .uniqueResult());
+    }
+
+    //通过phone找到User
     public static User findByPhone(String phone) {
         return Hib.query(session -> (User) session
                 .createQuery("from User where phone=:inPhone")
@@ -19,11 +29,53 @@ public class UserFactory {
                 .uniqueResult());
     }
 
+    //通过name找到User
     public static User findByName(String name) {
         return Hib.query(session -> (User) session
                 .createQuery("from User where name=:name")
                 .setParameter("name", name)
                 .uniqueResult());
+    }
+
+    /**
+     * 使用账号密码进行登录
+     * @param account    账号
+     * @param password   密码
+     * @return
+     */
+    public static User login(String account, String password){
+        final String accountStr = account.trim();
+        //对密码原文进行加密处理,以便于与数据库中的加密密码对比
+        final String encodePassword = encodePassword(password);
+
+        //查找
+        User user = Hib.query(session -> (User) session
+                .createQuery("from User  where phone=:phone and password=:password")
+                .setParameter("phone", accountStr)
+                .setParameter("password", encodePassword));
+
+        if (user != null){
+            //登录user并更新token
+            user = login(user);
+        }
+        return user;
+    }
+
+    /**
+     * 把一个验证通过的user进行登录操作
+     * @param user User
+     * @return user
+     */
+    private static User login(User user) {
+        //使用随机的UUID值充当token
+        String newToken = UUID.randomUUID().toString();
+        //进行Base64格式化
+        newToken = TextUtil.encodeBase64(newToken);
+        user.setToken(newToken);
+        return Hib.query(session -> {
+            session.saveOrUpdate(user);
+            return user;
+        });
     }
 
     /**
@@ -40,29 +92,35 @@ public class UserFactory {
         //处理密码
         password = encodePassword(password);
 
-        User user = new User();
-        user.setName(name);
-        user.setPassword(password);
-        user.setPhone(account);
-
-        //进行数据库存储
-        Session session = Hib.session();
-        session.beginTransaction();
-        try {
-            //保存
-            session.save(user);
-            session.getTransaction().commit();
-            return user;
-        } catch (Exception e) {
-            session.getTransaction().rollback();
-            return null;
+        User user = createUser(account, password, name);
+        if (user != null){
+            //注册成功自动登录
+            user = login(user);
         }
+        return user;
+    }
+
+    /**
+     * 注册部分新建一个用户,并存储至数据库
+     * @param account  账号
+     * @param password 密码
+     * @param name     用户名
+     * @return
+     */
+    private static User createUser(String account, String password, String name) {
+        User user = new User();
+        user.setPhone(account);
+        user.setPassword(password);
+        user.setName(name);
+
+        //数据库存储
+        return Hib.query(session -> (User) session.save(user));
     }
 
     /**
      * 对密码进行MD5加密之后进行Base64处理
      *
-     * @param password
+     * @param password 密码原文
      * @return
      */
     private static String encodePassword(String password) {
--- Web/IChatServer/src/main/java/cn/codekong/ichatserver/service/AccountService.java
+++ Web/IChatServer/src/main/java/cn/codekong/ichatserver/service/AccountService.java
@@ -6,8 +6,10 @@ import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 
+import cn.codekong.ichatserver.bean.api.account.AccountRspModel;
+import cn.codekong.ichatserver.bean.api.account.LoginModel;
 import cn.codekong.ichatserver.bean.api.account.RegisterModel;
-import cn.codekong.ichatserver.bean.card.UserCard;
+import cn.codekong.ichatserver.bean.api.base.ResponseModel;
 import cn.codekong.ichatserver.bean.db.User;
 import cn.codekong.ichatserver.factory.UserFactory;
 
@@ -19,37 +21,66 @@ import cn.codekong.ichatserver.factory.UserFactory;
 @Path("/account")
 public class AccountService {
 
+    /**
+     * 注册
+     *
+     * @param model
+     * @return
+     */
     @POST
     @Path("/register")
     //请求与返回的响应体为JSON
     @Consumes(MediaType.APPLICATION_JSON)
     @Produces(MediaType.APPLICATION_JSON)
-    public UserCard register(RegisterModel model) {
+    public ResponseModel<AccountRspModel> register(RegisterModel model) {
+        if (!RegisterModel.check(model)) {
+            //参数异常
+            return ResponseModel.buildParameterError();
+        }
+
         User user = UserFactory.findByPhone(model.getAccount().trim());
         if (user != null) {
-            UserCard card = new UserCard();
-            card.setName("已有了Phone");
-            return card;
+            //账户已存在
+            return ResponseModel.buildHaveAccountError();
         }
 
         user = UserFactory.findByName(model.getName().trim());
         if (user != null) {
-            UserCard card = new UserCard();
-            card.setName("已有了Name");
-            return card;
+            //已有用户名
+            return ResponseModel.buildHaveNameError();
         }
 
-        user = UserFactory.register(model.getAccount(), model.getPassword(), model.getName());
+        //开始注册逻辑
+        user = UserFactory.register(model.getAccount(),
+                model.getPassword(),
+                model.getName());
 
-        if (user != null){
-            UserCard card = new UserCard();
-            card.setName(user.getName());
-            card.setPhone(user.getPhone());
-            card.setSex(user.getSex());
-            card.setFollow(true);
-            card.setModifyAt(user.getUpdateAt());
-            return card;
+        if (user != null) {
+            AccountRspModel rspModel = new AccountRspModel(user);
+            return ResponseModel.buildOk(rspModel);
+        } else {
+            return ResponseModel.buildRegisterError();
         }
-        return null;
+    }
+
+    @POST
+    @Path("/login")
+    @Consumes(MediaType.APPLICATION_JSON)
+    @Produces(MediaType.APPLICATION_JSON)
+    public ResponseModel<AccountRspModel> login(LoginModel model) {
+       if (!LoginModel.check(model)){
+           //参数错误
+           return ResponseModel.buildParameterError();
+       }
+
+       User user = UserFactory.login(model.getAccount(), model.getPassword());
+       if (user != null){
+           //返回当前账户
+           AccountRspModel rspModel = new AccountRspModel(user);
+           return ResponseModel.buildOk(rspModel);
+       }else{
+           //登录失败
+           return ResponseModel.buildLoginError();
+       }
     }
 }
